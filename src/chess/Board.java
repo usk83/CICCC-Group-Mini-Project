090594f6ab77cbe95fdfe647fa52c0409a13d2e3
Naoki Mita <naoki.mita18@gmail.com>
2019-11-12 11:43:31 -0800

    Change initial board for removing position from piece

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -4,40 +4,6 @@ import java.lang.IllegalArgumentException;
 import chess.piece.*;
 
 public class Board {
-  private static final Piece[] INITIAL_PIECES = {
-    new Rook(Color.BLACK, 0, 0),
-    new Knight(Color.BLACK, 0, 1),
-    new Bishop(Color.BLACK, 0, 2),
-    new Queen(Color.BLACK, 0, 3),
-    new King(Color.BLACK, 0, 4),
-    new Bishop(Color.BLACK, 0, 5),
-    new Knight(Color.BLACK, 0, 6),
-    new Rook(Color.BLACK, 0, 7),
-    new Pawn(Color.BLACK, 1, 0),
-    new Pawn(Color.BLACK, 1, 1),
-    new Pawn(Color.BLACK, 1, 2),
-    new Pawn(Color.BLACK, 1, 3),
-    new Pawn(Color.BLACK, 1, 4),
-    new Pawn(Color.BLACK, 1, 5),
-    new Pawn(Color.BLACK, 1, 6),
-    new Pawn(Color.BLACK, 1, 7),
-    new Pawn(Color.WHITE, 6, 0),
-    new Pawn(Color.WHITE, 6, 1),
-    new Pawn(Color.WHITE, 6, 2),
-    new Pawn(Color.WHITE, 6, 3),
-    new Pawn(Color.WHITE, 6, 4),
-    new Pawn(Color.WHITE, 6, 5),
-    new Pawn(Color.WHITE, 6, 6),
-    new Pawn(Color.WHITE, 6, 7),
-    new Rook(Color.WHITE, 7, 0),
-    new Knight(Color.WHITE, 7, 1),
-    new Bishop(Color.WHITE, 7, 2),
-    new Queen(Color.WHITE, 7, 3),
-    new King(Color.WHITE, 7, 4),
-    new Bishop(Color.WHITE, 7, 5),
-    new Knight(Color.WHITE, 7, 6),
-    new Rook(Color.WHITE, 7, 7)
-  };
   private static final Piece[][] INITIAL_BOARD;
 
   private Piece[][] metrix;
@@ -45,10 +11,38 @@ public class Board {
 
   static {
     INITIAL_BOARD = new Piece[8][8];
-    for (Piece piece: INITIAL_PIECES) {
-      Position pos = piece.getPosition();
-      INITIAL_BOARD[pos.getRow()][pos.getCol()] = piece;
-    }
+    INITIAL_BOARD[0][0] = new Rook(Color.BLACK);
+    INITIAL_BOARD[0][1] = new Knight(Color.BLACK);
+    INITIAL_BOARD[0][2] = new Bishop(Color.BLACK);
+    INITIAL_BOARD[0][3] = new Queen(Color.BLACK);
+    INITIAL_BOARD[0][4] = new King(Color.BLACK);
+    INITIAL_BOARD[0][5] = new Bishop(Color.BLACK);
+    INITIAL_BOARD[0][6] = new Knight(Color.BLACK);
+    INITIAL_BOARD[0][7] = new Rook(Color.BLACK);
+    INITIAL_BOARD[1][0] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][1] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][2] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][3] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][4] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][5] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][6] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[1][7] = new Pawn(Color.BLACK);
+    INITIAL_BOARD[6][0] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][1] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][2] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][3] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][4] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][5] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][6] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[6][7] = new Pawn(Color.WHITE);
+    INITIAL_BOARD[7][0] = new Rook(Color.WHITE);
+    INITIAL_BOARD[7][1] = new Knight(Color.WHITE);
+    INITIAL_BOARD[7][2] = new Bishop(Color.WHITE);
+    INITIAL_BOARD[7][3] = new Queen(Color.WHITE);
+    INITIAL_BOARD[7][4] = new King(Color.WHITE);
+    INITIAL_BOARD[7][5] = new Bishop(Color.WHITE);
+    INITIAL_BOARD[7][6] = new Knight(Color.WHITE);
+    INITIAL_BOARD[7][7] = new Rook(Color.WHITE);
   }
 
   public Board() {
