2a1db435ceaf7248b2fdc21ada76915ae121ecd9
Naoki Mita <naoki.mita18@gmail.com>
2019-11-13 21:35:19 -0800

    Set some private variable and isValidMove in Pawn.java

--- src/chess/piece/Pawn.java
+++ src/chess/piece/Pawn.java
@@ -9,8 +9,20 @@ public class Pawn extends Piece {
     put(Color.BLACK, '♟');
   }};
 
+  // if this pawn is moved, isFirstMoved turn out true but at next your own turn, it turn out false
+  // this variable is related to "en passant"
+  // ex. n:false -> n + 1:true -> n + 2:false
+  private boolean isFirstMoved;
+
+  // if this pawn is moved, isMoved turn out true and it never change again
+  // when the value is true, this Pawn can move forward two squares.
+  // ex. n:false -> n + 1:true
+  private boolean isMoved;
+
   public Pawn(Color color) {
     super(color, symbols);
+    this.isFirstMoved = false;
+    this.isMoved = false;
   }
 
   @Override
@@ -18,7 +30,20 @@ public class Pawn extends Piece {
     if (!super.isValidMove(row, col, isEnemyPiece)) {
       return false;
     }
-    // ToDo: implement
-    return true;
+
+    // if arguments are impossible int
+    // row = 1 or 2, col = 0 or 1
+    if (row == 0 || 3 <= row || 2 <= col) return false;
+
+    // move to capture the diagonal forward opponent piece, or "en passant"
+    if (row == 1 && col == 1) return true;
+
+    // move forward
+    if (col == 0) {
+      if (!isMoved && row == 2) return true;
+      if (row == 1) return true;
+    }
+
+    return false;
   }
 }
