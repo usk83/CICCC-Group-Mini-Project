39b2e6a48fe9cc19c112ea171c2cb2aa739d2050
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-11-15 16:33:43 -0800

    Improve RESIGN action
    also fix indentation

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -23,60 +23,62 @@ public class Game {
         + "( (__ ) __ ( ) _) \\___ \\\\___ \\  ( (_ \\/    \\/ \\/ \\ ) _) \\_/\n"
         + " \\___)\\_)(_/(____)(____/(____/   \\___/\\_/\\_/\\_)(_/(____)(_)\n"
         + "\n");
-      boolean isGameOnGoing = true;
-      while (isGameOnGoing) {
-        System.out.println("Turn of : " + turn);
-        String userInput = InputController.getUserInput("Enter UCI (type 'help' for help): ");
-        switch (Command.parse(userInput)) {
-          case HELP:
-            System.out.println("* type 'help' for help \n " +
-                "* type 'board' to see the board again \n " +
-                "* type 'resign' to resign \n " +
-                "* type 'moves' to list all possible moves \n " +
-                "* type a square (e.g. b1, e2) to list possible moves for that square \n " +
-                "* type a UCI (e.g. b1c3, e7e8q) to make a move");
-            break;
-          case BOARD:
-            System.out.println(board);
-            break;
-          case RESIGN:
-            int whitewons = 0;
-            int blackwons = 0;
-            String winner = "";
-            switch (turn) {
-              case BLACK:
-                winner = " WHITES ";
-                whitewons++;
-                break;
-              case WHITE:
-                winner = " BLACKS ";
-                blackwons++;
-                break;
-            }
-            System.out.println("GAME OVER " + whitewons + " - " + blackwons + winner + "WON BY RESIGNATION");
-            isGameOnGoing = false;
-            break;
-          case GO_MOVE:
-            int[] moveTo = new int[4];
-            char promotionPiece;
-            int index = 0;
-            for (int i = 0; i < userInput.length(); i++) {
-              char c = userInput.charAt(i);
-              moveTo[index] = Character.isDigit(c) ? convertBoardNumber(c) : convertLetterToNumber(c);
-              index++;
-            }
-            board.update(new Position(moveTo[0], moveTo[1]), new Position(moveTo[2], moveTo[3]));
 
-            switch (turn) {
-              case WHITE:
-                turn = Color.BLACK;
-                break;
-              case BLACK:
-                turn = Color.WHITE;
-                break;
-            }
-        }
+    boolean isGameOnGoing = true;
+    while (isGameOnGoing) {
+      System.out.println("Turn of : " + turn);
+      String userInput = InputController.getUserInput("Enter UCI (type 'help' for help): ");
+      switch (Command.parse(userInput)) {
+        case HELP:
+          System.out.println("* type 'help' for help \n " +
+              "* type 'board' to see the board again \n " +
+              "* type 'resign' to resign \n " +
+              "* type 'moves' to list all possible moves \n " +
+              "* type a square (e.g. b1, e2) to list possible moves for that square \n " +
+              "* type a UCI (e.g. b1c3, e7e8q) to make a move");
+          break;
+        case BOARD:
+          System.out.println(board);
+          break;
+        case RESIGN:
+          String score = "";
+          switch (turn) {
+            case BLACK:
+              score = "1 - 0";
+              break;
+            case WHITE:
+              score = "0 - 1";
+              break;
+          }
+          switchTurn();
+          System.out.printf("GAME OVER %s %s WON BY RESIGNATION\n", score, turn);
+          isGameOnGoing = false;
+          break;
+        case GO_MOVE:
+          int[] moveTo = new int[4];
+          char promotionPiece;
+          int index = 0;
+          for (int i = 0; i < userInput.length(); i++) {
+            char c = userInput.charAt(i);
+            moveTo[index] = Character.isDigit(c) ? convertBoardNumber(c) : convertLetterToNumber(c);
+            index++;
+          }
+          board.update(new Position(moveTo[0], moveTo[1]), new Position(moveTo[2], moveTo[3]));
+          switchTurn();
+          break;
       }
+    }
+  }
+
+  private void switchTurn() {
+    switch (turn) {
+      case WHITE:
+        turn = Color.BLACK;
+        break;
+      case BLACK:
+        turn = Color.WHITE;
+        break;
+    }
   }
 
   //  https://stackoverflow.com/questions/15027231/java-how-to-convert-letters-in-a-string-to-a-number
