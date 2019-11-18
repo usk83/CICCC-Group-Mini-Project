e47e8394e9d9fef265f2072e6e122381eb59a681
Naoki Mita <naoki.mita18@gmail.com>
2019-11-17 16:13:06 -0800

    Rename a variable to getAttackablePieces in SquareManageable.java

--- src/chess/piece/SquareManageable.java
+++ src/chess/piece/SquareManageable.java
@@ -4,5 +4,5 @@ public interface SquareManageable {
   public Piece get(int row, int col);
   public void update(int fromRow, int fromCol, int toRow, int toCol);
   public void remove(int row, int col);
-  public Piece[] getAttackedList(int row, int col);
+  public Piece[] getAttackablePieces(int row, int col);
 }
